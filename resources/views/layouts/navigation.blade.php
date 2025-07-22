bd85597793e65b4ae0b8dbd9f579b9b050e7d468
Stefano Chermaz <stefano@crowdm.com>
2025-07-22 07:54:16 +0200

    feat: reorder navigation menu - move Punti Pesca before Uscite
    - Changed navigation order in both desktop and mobile menus
    - New order: Dashboard -> Punti Pesca -> Uscite -> Le Mie Catture -> Mappa -> Statistiche
    - Updated both nav-link and responsive-nav-link sections
    - Maintains consistency across all navigation views

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -35,18 +35,18 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     
-                    <x-nav-link :href="route('fishing-trips.index')" :active="request()->routeIs('fishing-trips.*')" class="nav-link">
-                        <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                            <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                        </svg>
-                        {{ __('Uscite') }}
-                    </x-nav-link>
-                    
                     <x-nav-link :href="route('fishing-spots.index')" :active="request()->routeIs('fishing-spots.*')" class="nav-link">
                         <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                             <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"/>
                         </svg>
                         {{ __('Punti Pesca') }}
+                    </x-nav-link>
+                    
+                    <x-nav-link :href="route('fishing-trips.index')" :active="request()->routeIs('fishing-trips.*')" class="nav-link">
+                        <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                            <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                        </svg>
+                        {{ __('Uscite') }}
                     </x-nav-link>
                     
                     <x-nav-link :href="route('catches.index')" :active="request()->routeIs('catches.*')" class="nav-link">
@@ -263,18 +263,18 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
             
-            <x-responsive-nav-link :href="route('fishing-trips.index')" :active="request()->routeIs('fishing-trips.*')" class="nav-link">
-                <svg class="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                    <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                </svg>
-                {{ __('Uscite') }}
-            </x-responsive-nav-link>
-            
             <x-responsive-nav-link :href="route('fishing-spots.index')" :active="request()->routeIs('fishing-spots.*')" class="nav-link">
                 <svg class="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
                     <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"/>
                 </svg>
                 {{ __('Punti Pesca') }}
+            </x-responsive-nav-link>
+            
+            <x-responsive-nav-link :href="route('fishing-trips.index')" :active="request()->routeIs('fishing-trips.*')" class="nav-link">
+                <svg class="w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
+                    <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                </svg>
+                {{ __('Uscite') }}
             </x-responsive-nav-link>
             
             <x-responsive-nav-link :href="route('catches.index')" :active="request()->routeIs('catches.*')" class="nav-link">
